471d301297cea685db5ccf644924b80657dab505
princevora <princevora684@gmail.com>
2025-03-23 17:40:26 +0530

    update middleware

--- app/Http/Middleware/AdminApiMiddleware.php
+++ app/Http/Middleware/AdminApiMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Admin;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,6 +16,24 @@ class AdminApiMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
+        // get api token
+        $api_token = $request->bearerToken();
+        
+        if(is_null($api_token)){
+            return response()->json([
+                'status' => 403,
+                'message' => 'Invalid api key'
+            ], 403);
+        }
+        
+        // check if the token is valid
+        if(!Admin::where('api_token', $api_token)->exists()){
+            return response()->json([
+                'status' => 403,
+                'message' => 'Invalid api key'
+            ], 403);
+        }
+
         return $next($request);
     }
 }
